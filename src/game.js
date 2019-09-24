ab19c6fd71bfbfc9320fe43adbcd2bc75eb32db5
Hector Alfaro <hectorsector@github.com>
2019-09-24 05:59:57 -0400

    add a Bananas name

--- src/game.js
+++ src/game.js
@@ -1,7 +1,7 @@
 export default class Game {
   constructor (p1, p2) {
     this.p1 = p1
-    this.p2 = p2
+    this.p2 = 'Bananas'
     this.board = [[null, null, null], [null, null, null], [null, null, null]]
     this.player = Math.random() < 0.5 ? this.p1 : this.p2
     this.sym = 'X'
