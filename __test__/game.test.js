4d6e5f12584b7746fb5d41640b201a000fe78144
Hector Alfaro <hectorsector@github.com>
2019-10-16 13:48:24 -0400

    add snapshots to tests

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -1,4 +1,15 @@
 const Game = require('../src/game').default
+const fs = require('fs')
+
+describe('App', () => {
+  it('Contains the compiled JavaScript', async (done) => {
+    fs.readFile('./public/main.js', 'utf8', (err, data) => {
+      expect(err).toBe(null)
+      expect(data).toMatchSnapshot()
+      done()
+    })
+  })
+})
 
 describe('Game', () => {
   let game, p1, p2
